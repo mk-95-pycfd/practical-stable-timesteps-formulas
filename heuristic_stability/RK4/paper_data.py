71c61bc6349e5a870792b9842c384a284f5b673e
mk-95 <karammokbel@gmail.com>
2022-03-02 16:02:32 -0700

    added the results for the unified formulation

--- heuristic_stability/RK4/paper_data.py
+++ heuristic_stability/RK4/paper_data.py
@@ -31,24 +31,36 @@ markers = ['o', 's', 'D', 'v','*']
 
 # load data
 stability_data = np.loadtxt('./RK4-3-8-stability.txt',delimiter=",",usecols=[0,1])
-#convert data
-stability_paper = stability_data/np.sqrt(2)
+#convert data to match the main results section
+# stability_paper = stability_data/np.sqrt(2)
+
+#convert data to match the unified formulation section
+stability_paper = stability_data*1
+# Reh conversion
+stability_paper[:,1] = stability_data[:,1]/np.sqrt(2)/2
+# Courant conversion
+stability_paper[:,0] = stability_data[:,0]*np.sqrt(2)
 
 #plot
 Reh = stability_paper[2:,1]
 Courant = stability_paper[2:,0]
 ax.loglog(Reh, Courant, color=colors[1], linewidth=1.5, marker=markers[0], markersize=4.5, label="RK4 Taylor Vortex")
-# plot the analytical stability boundaries
-lower_bound = lambda Pe: Pe/2.0/1.45
-upper_bound = lambda Pe: 2.0/np.sqrt(2)*np.ones_like(Pe)
+## plot the analytical stability boundaries
+# inner_bound = lambda Pe: Pe/2.0/1.59
+# outer_bound = lambda Pe: 3.0/2/np.sqrt(3)*np.ones_like(Pe)
 
-ax.loglog(Reh, lower_bound(Reh),'--',color=colors[2],linewidth=1.5,label="Lower Bound")
-ax.loglog(Reh, upper_bound(Reh),'-.',color=colors[3],linewidth=1.5,label="Upper Bound")
+## plot the unified stability formulation boundaries
+inner_bound = lambda Pe: 2.09*Pe
+outer_bound = lambda Pe: 2.84*np.ones_like(Pe)
+
+ax.loglog(Reh, inner_bound(Reh),'--',color=colors[2],linewidth=1.5,label="Inner Bound")
+ax.loglog(Reh, outer_bound(Reh),'-.',color=colors[3],linewidth=1.5,label="Outer Bound")
+
 
 ax.set_ylabel('Courant', fontsize=12)
 ax.set_xlabel('Cell Reynolds Number', fontsize=12)
 
-ax.set_ylim([4e-2,2])
+# ax.set_ylim([4e-2,2])
 plt.legend(ncol=1,loc='lower right',fontsize=10,frameon=False)
 
 plt.tight_layout()
